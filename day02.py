416b1577422fbceb58132ce60ad7ee277a2195d4
jahearn3 <josephahearn3@gmail.com>
2025-12-02 09:53:01 -0800

    Day 2 complete

---
+++ day02.py
@@ -0,0 +1,42 @@
+# Day 2: Gift Shop
+
+import load_data as ld
+import os
+
+f = os.path.basename(__file__)
+day = f[3:5]
+
+data = ld.load_data(f"example{day}.txt")
+data = ld.load_data(f"input{day}.txt")
+
+# Part 1
+ans = 0
+
+id_ranges = data[0].split(',')
+for idr in id_ranges:
+    first_id, last_id = idr.split('-')
+    for i in range(int(first_id), int(last_id) + 1):
+        id = str(i)
+        first_half, second_half = id[:len(id)//2], id[len(id)//2:]
+        if first_half == second_half:
+            ans += i
+
+print(ans)
+
+# Part 2
+invalid_ids = []
+
+id_ranges = data[0].split(',')
+for idr in id_ranges:
+    first_id, last_id = idr.split('-')
+    for i in range(int(first_id), int(last_id) + 1):
+        id = str(i)
+        for j in range(2, len(id) + 1):
+            if len(id) % j == 0:
+                length = len(id) // j
+                pieces = [id[k * length:(k + 1) * length] for k in range(j)]
+                all_equal = all(piece == pieces[0] for piece in pieces)
+                if all_equal:
+                    invalid_ids.append(i)
+
+print(sum(set(invalid_ids)))
