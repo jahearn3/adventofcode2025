4b718cb96c11bd8f629078f47a1c388d47148c30
jahearn3 <josephahearn3@gmail.com>
2025-12-09 09:03:50 -0800

    Day 6 part 1

---
+++ day06.py
@@ -0,0 +1,27 @@
+# Day 6: Trash Compactor
+
+import load_data as ld
+import os
+from math import prod
+
+f = os.path.basename(__file__)
+day = f[3:5]
+
+data = ld.load_data(f"example{day}.txt")
+data = ld.load_data(f"input{day}.txt")
+
+numbers = []
+for i, line in enumerate(data):
+    if i == len(data) - 1:
+        operations = line.split()
+    else:
+        numbers.append([int(x) for x in line.split()])
+transposed = list(map(list, zip(*numbers)))
+totals = []
+for i in range(len(transposed)):
+    if operations[i] == '+':
+        totals.append(sum(transposed[i]))
+    elif operations[i] == '*':
+        totals.append(prod(transposed[i]))
+
+print(sum(totals))
