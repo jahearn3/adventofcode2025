46f3e0a25850018e5c9427a75968168e135fc1d6
jahearn3 <josephahearn3@gmail.com>
2025-12-30 12:08:09 -0800

    Day 10 complete

--- day10.py
+++ day10.py
@@ -3,6 +3,7 @@
 import load_data as ld
 import os
 import itertools
+import z3
 
 f = os.path.basename(__file__)
 day = f[3:5]
@@ -16,7 +17,6 @@ for line in data:
     schematics = line.split(' ')
     indicators = schematics[0].strip('[]')
     buttons = [x.strip('()') for x in schematics[1:-1]]
-    joltages = schematics[-1].strip('{}')  # Not needed for part 1
     end_state = {idx for idx, light in enumerate(indicators) if light == "#"}
     buttons = [set(map(int, button.split(","))) for button in buttons]
     for count in range(1, len(buttons) + 1):
@@ -30,5 +30,31 @@ for line in data:
         else:  # if a match was not found
             continue  # go to next iteration of count
         break  # if a match was found
+
+print(total)
+
+# Part 2
+# Integer Programming
+total = 0
+
+for line in data:
+    schematics = line.split(' ')
+    buttons = [x.strip('()') for x in schematics[1:-1]]
+    joltages = schematics[-1].strip('{}')
+    buttons = [set(map(int, button.split(','))) for button in buttons]
+    joltages = list(map(int, joltages.split(',')))
+    o = z3.Optimize()
+    vars = z3.Ints(f'n{i}' for i in range(len(buttons)))
+    for var in vars:
+        o.add(var >= 0)
+    for i, joltage in enumerate(joltages):
+        equation = 0
+        for b, button in enumerate(buttons):
+            if i in button:
+                equation += vars[b]
+        o.add(equation == joltage)
+    o.minimize(sum(vars))
+    o.check()
+    total += o.model().eval(sum(vars)).as_long()
 
 print(total)
